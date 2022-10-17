8b98678ba4478eb7d52d8bb2b7629280dc61aa92
Joshua Oh <joshua.jy.oh@gmail.com>
2022-10-16 22:07:15 -0400

    integrate vue into demo features

--- app.py
+++ app.py
@@ -20,38 +20,63 @@ def toHTML(table, includeCols=False):
 
 @app.route("/")
 def index_site():
-    cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='test')
-    cursor = cnx.cursor()
-    cursor.execute('use test')
-    cursor.execute('select * from t;')
-    l = []
-    for row in cursor:
-        l.append(row)
-    cnx.close()
-
     with open('index.html', 'r') as f:
         site = f.read()
 
-    return site + 'Current state of t: ' + toHTML(l, True)
+    return site
+
+#@app.route("/")
+#def index_site():
+#    cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='test')
+#    cursor = cnx.cursor()
+#    cursor.execute('use test')
+#    cursor.execute('select * from t;')
+#    l = []
+#    for row in cursor:
+#        l.append(row)
+#    cnx.close()
+#
+#    with open('index.html', 'r') as f:
+#        site = f.read()
+#
+#    return site + 'Current state of t: ' + toHTML(l, True)
 
 @app.get("/query")
 def query():
-    back_button = '<br/><a href="/"><button>Go back</button></a>'
     args = request.args
     q = args['query']
     if 'drop' in q.lower():
-        return 'no' + back_button
+        return 'no'
     cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='test')
     cursor = cnx.cursor()
     try:
         cursor.execute(q)
     except:
-        return 'bad query' + back_button
+        return 'bad query'
     l = []
     for row in cursor:
         l.append(row)
     cnx.close()
-    return toHTML(l) + back_button
+    return l
+
+#@app.get("/query")
+#def query():
+#    back_button = '<br/><a href="/"><button>Go back</button></a>'
+#    args = request.args
+#    q = args['query']
+#    if 'drop' in q.lower():
+#        return 'no' + back_button
+#    cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='test')
+#    cursor = cnx.cursor()
+#    try:
+#        cursor.execute(q)
+#    except:
+#        return 'bad query' + back_button
+#    l = []
+#    for row in cursor:
+#        l.append(row)
+#    cnx.close()
+#    return toHTML(l) + back_button
 
 @app.get("/reset")
 def reset():
--- static/src/vue-app.js
+++ static/src/vue-app.js
@@ -1,4 +1,5 @@
 import * as Vue from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { Controller } from './controller.js';
 
 class VueApp {
     static #app = null;
@@ -8,8 +9,25 @@ class VueApp {
         this.#app = Vue.createApp({
             data() {
                 return {
-                    state: 'demo'
+                    state: 'demo',
+                    demoCurrentStateData: [],
+                    demoQueryData: [],
+                    inDemoQuery: '',
                 }
+            },
+            methods: {
+                getDemoQuery(query) {
+                    Controller.getDemoQuery(query)
+                    .then((data) => {
+                        this.demoQueryData = data;
+                    });
+                }
+            },
+            mounted() {
+                Controller.getDemoData()
+                .then((data) => {
+                    this.demoCurrentStateData = data;
+                });
             }
         });
         this.#app.mount(`#${this.#APP_ID}`);
