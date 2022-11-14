f664f0a873ba5a747cda5c7862afe89ef2740038
Joshua Oh <joshua.jy.oh@gmail.com>
2022-11-14 17:49:54 -0500

    update more front-end template values

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import mysql.connector
 from flask import Flask, request
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 @app.route("/")
 def index_site():
