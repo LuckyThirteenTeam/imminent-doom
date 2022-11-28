38e468782af9c8ecb014febf66d7c72bdee5195c
Droplet Server <>
2022-11-28 05:28:53 +0000

    implemented login

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import mysql.connector
-from flask import Flask, request
+from flask import Flask, request, session
 from flask_cors import CORS
+from random import randint
+from hashlib import sha256
 
 app = Flask(__name__)
 CORS(app)
+
+f = open('.secret_key')
+secret_key = f.read().strip()
+f.close()
+app.secret_key = secret_key
 
 @app.route("/")
 def index_site():
@@ -22,7 +29,6 @@ def sample():
 @app.get("/query")
 def query():
     args = request.args
-    print(args)
     q = args['query']
     if 'drop' in q.lower():
         return 'no'
@@ -30,6 +36,121 @@ def query():
     cursor = cnx.cursor()
     try:
         cursor.execute(q)
+    except Exception as e:
+        return '[[%s]]' % str(e)
+
+    l = []
+    for row in cursor:
+        l.append(row)
+    cnx.close()
+    return l
+
+def generate_salt():
+    s = ''
+    valid_chars = list(range(65, 91)) + list(range(48, 58)) + list(range(97, 123))
+    n = len(valid_chars)
+    for i in range(32):
+        s += chr(valid_chars[randint(0, n - 1)])
+    return s
+
+def read_pepper():
+    f = open('.pepper')
+    pepper = f.read().strip()
+    f.close()
+    return pepper
+
+@app.post("/signup")
+def signup():
+    args = request.args
+    if 'username' not in args:
+        return 'Username Required', 401
+    if 'password' not in args:
+        return 'Password Required', 401
+
+    username = args['username']
+    password = args['password']
+
+    cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='production')
+    cursor = cnx.cursor()
+
+    try:
+        cursor.execute('SELECT 1 FROM User WHERE username = %s', [username])
+    except Exception as e:
+        return '[[%s]]' % str(e)
+
+    l = []
+    for row in cursor:
+        l.append(row)
+    if len(l) > 0:
+        return 'User Already Exists', 401
+
+    salt = generate_salt()
+    pepper = read_pepper()
+    hashed = sha256((password + salt + pepper).encode()).hexdigest()
+
+    try:
+        cursor.execute('INSERT INTO User VALUES (%s, %s, %s)', [username, hashed, salt])
+        cnx.commit()
+    except Exception as e:
+        return '[[%s]]' % str(e)
+
+    cnx.close()
+    return 'Signed Up'
+
+
+@app.post("/login")
+def login():
+    args = request.args
+    if 'username' not in args:
+        return 'Username Required', 401
+    if 'password' not in args:
+        return 'Password Required', 401
+    username = args['username']
+    password = args['password']
+
+    cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='production')
+    cursor = cnx.cursor()
+
+    try:
+        cursor.execute('SELECT salt FROM User WHERE username = %s', [username])
+    except Exception as e:
+        return '[[%s]]' % str(e)
+
+    l = []
+    for row in cursor:
+        l.append(row)
+    if len(l) != 1:
+        return 'User Not Found', 401
+    salt = l[0][0]
+    pepper = read_pepper()
+
+    hashed = sha256((password + salt + pepper).encode()).hexdigest()
+
+    try:
+        cursor.execute('SELECT 1 FROM User WHERE username = %s AND password = %s', [username, hashed])
+    except Exception as e:
+        return '[[%s]]' % str(e)
+
+    l = []
+    for row in cursor:
+        l.append(row)
+    if len(l) != 1:
+        return 'Incorrect Password', 401
+
+    cnx.close()
+    session['username'] = username
+    return 'Logged In'
+
+@app.get("/saved_locations")
+def saved_locations():
+    if 'username' not in session:
+        return 'Not Logged In', 401
+    username = session['username']
+
+    cnx = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='production')
+    cursor = cnx.cursor()
+    try:
+        cursor.execute('SELECT locationId FROM SavedLocation WHERE username = %s', [username])
     except Exception as e:
         return '[[%s]]' % str(e)
 
